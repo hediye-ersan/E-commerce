e07619906914024508bad8670cc281696259c841
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-22 14:48:49 +0300

    Reorder components in App to improve layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import { Switch } from 'react-router-dom/cjs/react-router-dom';
 import NavBar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import NewArrivals from './components/NewArrivals.jsx';
 
 
 function App() {
 
-  
+
 
   return (
     <>
       <Switch>
         <Route>
           <NavBar />
-          <Footer/>
+          <Header/>
+          <NewArrivals />
+          <Footer />
         </Route>
       </Switch>
 
