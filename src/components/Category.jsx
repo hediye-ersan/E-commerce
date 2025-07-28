1743835f56625914f2791281b6cfa3c63567e419
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-28 16:43:16 +0300

    created new Category.jsx

---
+++ src/components/Category.jsx
@@ -0,0 +1,52 @@
+import { Link, useLocation } from "react-router-dom";
+import ProductCard from "../components/ProductCard"; // Statik ürün için bir kart bileşeni
+import { ChevronRight, SlidersHorizontal } from "lucide-react";
+import React from "react";
+
+function Category(){
+    const location = useLocation();
+  const path = location.pathname.split("/")[2]; // /category/men veya /category/women
+
+  const category = path.charAt(0).toUpperCase() + path.slice(1); // 'Men' ya da 'Women'
+
+    return (
+    <div className="p-6">
+      {/* Breadcrumb */}
+      <div className="text-sm text-gray-600 flex items-center gap-1">
+        <Link to="/" className="hover:underline">Home</Link>
+        <ChevronRight size={16} />
+        <span>Shop</span>
+        <ChevronRight size={16} />
+        <span className="capitalize">{category}</span>
+      </div>
+
+      {/* Title */}
+      <div className="flex justify-between items-center mt-4 mb-2">
+        <h2 className="text-3xl font-bold">{category}</h2>
+        <span className="text-gray-500">Showing 1-10 of 100 Products</span>
+        <SlidersHorizontal />
+      </div>
+
+      
+
+      {/* Products */}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {[...Array(8)].map((_, index) => (
+          <ProductCard key={index} />
+        ))}
+      </div>
+
+      {/* Pagination */}
+      <div className="flex justify-between items-center mt-6">
+        <button className="px-4 py-2 border rounded">Previous</button>
+        <div className="flex gap-2">
+          <button className="w-8 h-8 border rounded bg-black text-white">1</button>
+          <button className="w-8 h-8 border rounded">2</button>
+          <button className="w-8 h-8 border rounded">3</button>
+        </div>
+        <button className="px-4 py-2 border rounded">Next</button>
+      </div>
+    </div>
+  );
+}
+export default Category;
