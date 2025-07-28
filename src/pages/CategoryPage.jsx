015179dc6d584baa58bab4dae0d3ec4ce2f838b7
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-28 16:46:11 +0300

    CategoryPage.jsx was created by adding components.

---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Category from "../components/Category"; // Statik kategori bileşeni
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const CategoryPage = () => {
+
+  return (
+    <>
+    
+    <NavBar />
+    <Category/>
+    <Footer />
+    </>
+
+  );
+};
+
+export default CategoryPage;
