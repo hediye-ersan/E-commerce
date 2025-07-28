367ca5b8b100e9b4f31c1af3faab8b30530e48fb
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-28 16:45:21 +0300

    Add CategoryPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './reset.css'
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import { Switch } from 'react-router-dom/cjs/react-router-dom';
 import HomePage from './pages/HomePage.jsx';
+import CategoryPage from './pages/CategoryPage.jsx';
 
 
 function App() {
@@ -12,10 +13,9 @@ function App() {
 
   return (
     <>
-      <Switch>
-        <Route>
-          <HomePage/>
-        </Route>
+       <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/category/:category" component={CategoryPage} />
       </Switch>
 
     </>
