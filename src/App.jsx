04153c839c47781d26db0d61c27072dbd8a91307
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-31 16:20:08 +0300

    Adds the product detail page to Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import { Switch } from 'react-router-dom/cjs/react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import CategoryPage from './pages/CategoryPage.jsx';
+import ProductDetailPage from './pages/ProductDetailPage.jsx'; // Yeni sayfa bileşeni
 
 
 function App() {
@@ -13,9 +14,10 @@ function App() {
 
   return (
     <>
-       <Switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/category/:category" component={CategoryPage} />
+        <Route path="/product/:id" component={ProductDetailPage} />
       </Switch>
 
     </>
