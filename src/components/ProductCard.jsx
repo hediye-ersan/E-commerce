f00a806c06be672107e2264dac34365c4918498f
Hediye Ersan <hediyesnl9@gmail.com>
2025-08-01 13:08:27 +0300

    Revises styles for product information

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,26 +4,29 @@ import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
-    <Link to="/product/1" className="block p-3 border rounded-md hover:shadow">
-      <div className="border rounded p-4">
+    <Link to="/product/1" className="block border border-gray-200 rounded-md hover:shadow">
+      <div className="rounded p-2">
         <img
-          src="https://picsum.photos/250/250"
+          src="https://picsum.photos/300/300"
           alt="shirt"
-          className="w-full h-48 object-cover mb-2"
+          className=" w-full h-full object-cover"
         />
-        <h2 className="font-semibold text-lg">Black Striped T-shirt</h2>
-        <p className="text-sm text-gray-500">$120</p>
+      </div>
+      <div className="px-2">
+      <h2 className="font-semibold text-sm">Black Striped T-shirt</h2>
+      <p className="text-sm text-gray-500">$120</p>
       </div>
 
-      <h3 className="font-semibold text-sm mb-1">Polo with Tipping Detail</h3>
-      <div className="flex items-center text-yellow-500 text-sm mb-1">
+
+      
+      <div className="flex items-center text-yellow-500 text-sm px-2">
         {[...Array(4)].map((_, i) => (
           <Star key={i} size={14} fill="currentColor" />
         ))}
         <span className="text-gray-500 ml-1 text-xs">4.5/5</span>
       </div>
-      <div className="flex items-center gap-2">
-        <span className="font-bold text-sm">$180</span>
+      <div className="flex items-center gap-2 px-2 pb-2">
+        <span className="font-bold text-xl">$180</span>
         <span className="line-through text-gray-400 text-sm">$242</span>
         <span className="text-red-500 text-xs">-20%</span>
       </div>
