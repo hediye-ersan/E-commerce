0779a9498ae296f2bb081fc412137a8b6528e40a
Hediye Ersan <hediyesnl9@gmail.com>
2025-08-02 18:08:46 +0300

    Integrate Cart component into App, wrapping routes with CartProvider for context management.

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,20 @@ import { Switch } from 'react-router-dom/cjs/react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import CategoryPage from './pages/CategoryPage.jsx';
 import ProductDetailPage from './pages/ProductDetailPage.jsx'; // Yeni sayfa bileşeni
+import Cart from './pages/Cart.jsx';
+import { CartProvider } from './contexts/CartContext';
 
 
 function App() {
-
-
-
   return (
-    <>
+    <CartProvider>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/category/:category" component={CategoryPage} />
         <Route path="/product/:id" component={ProductDetailPage} />
+        <Route path="/cart" component={Cart} />
       </Switch>
-
-    </>
+    </CartProvider>
   )
 }
 export default App
