aa73f362d41eb2c58aa9fa3f9b4e0bf79c639376
hediye-ersan <hediyesnl9@gmail.com>
2025-08-13 16:18:11 +0300

    added OrderHistoryProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 
-import { useState } from 'react';
 import './reset.css'
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
-import { Switch } from 'react-router-dom/cjs/react-router-dom';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
 import CategoryPage from './pages/CategoryPage.jsx';
-import ProductDetailPage from './pages/ProductDetailPage.jsx'; // Yeni sayfa bileşeni
+import ProductDetailPage from './pages/ProductDetailPage.jsx';
 import Cart from './pages/Cart.jsx';
 import FavoritesPage from './pages/FavoritesPage.jsx';
+import OrderHistoryPage from './pages/OrderHistoryPage.jsx';
 import { CartProvider } from './contexts/CartContext';
-
+import { OrderHistoryProvider } from './contexts/OrderHistoryContext';
 
 function App() {
   return (
     <CartProvider>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/category/:category" component={CategoryPage} />
-        <Route path="/product/:id" component={ProductDetailPage} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/favorites" component={FavoritesPage} />
-      </Switch>
+      <OrderHistoryProvider>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/category/:category" component={CategoryPage} />
+          <Route path="/product/:id" component={ProductDetailPage} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/favorites" component={FavoritesPage} />
+          <Route path="/orders" component={OrderHistoryPage} />
+        </Switch>
+      </OrderHistoryProvider>
     </CartProvider>
   )
 }
