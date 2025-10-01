b4bfbc59c1e188aa09b6695b1f3c38f6d8403b6f
hediye-ersan <hediyesnl9@gmail.com>
2025-10-01 14:06:10 +0300

    Adds the data in the filter to this file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,32 @@
+export const CATEGORIES = ["T-shirts", "Shorts", "Shirts", "Hoodie", "Jeans", "Dresses", "Pants"];
+
+export const COLOR_OPTIONS = [
+  { name: "green", value: "#22c55e" },
+  { name: "red", value: "#ef4444" },
+  { name: "yellow", value: "#eab308" },
+  { name: "orange", value: "#f97316" },
+  { name: "cyan", value: "#06b6d4" },
+  { name: "blue", value: "#3b82f6" },
+  { name: "purple", value: "#a855f7" },
+  { name: "pink", value: "#ec4899" },
+  { name: "white", value: "#ffffff" },
+  { name: "black", value: "#000000" },
+];
+
+export const SIZE_OPTIONS = [
+  "XX-Small",
+  "X-Small",
+  "Small",
+  "Medium",
+  "Large",
+  "X-Large",
+  "XX-Large",
+  "3X-Large",
+  "4X-Large",
+];
+
+export const STYLE_OPTIONS = ["Casual", "Formal", "Party", "Gym"];
+
 
 export const DISCOUNT_RATE = 0.2; // 20% discount
 export const DELIVERY_FEE = 15;
